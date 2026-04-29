dd4cdcc0e1c4168f12024266490d822231407dec
47krishnaveersingh <krishna.vs12345@gmail.com>
2026-04-29 18:54:05 +0530

    feat:connected advance fraud detection with postgres

--- spark/jobs/streaming_job.py
+++ spark/jobs/streaming_job.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, from_json
+from pyspark.sql.functions import col, from_json, window, count, to_timestamp, lit, from_unixtime
 from pyspark.sql.types import StructType, StringType, DoubleType
 
 spark = SparkSession.builder \
@@ -8,6 +8,20 @@ spark = SparkSession.builder \
     .getOrCreate()
 
 spark.sparkContext.setLogLevel("WARN")
+
+def write_to_postgres(batch_df, batch_id):
+    if batch_df.rdd.isEmpty():
+        return
+
+    batch_df.write \
+        .format("jdbc") \
+        .option("url", "jdbc:postgresql://postgres:5432/frauddb") \
+        .option("dbtable", "fraud_transactions") \
+        .option("user", "admin") \
+        .option("password", "admin") \
+        .option("driver", "org.postgresql.Driver") \
+        .mode("append") \
+        .save()
 
 schema = StructType() \
     .add("user_id", StringType()) \
@@ -29,12 +43,44 @@ json_df = df.selectExpr("CAST(value AS STRING)")
 parsed_df = json_df.select(
     from_json(col("value"), schema).alias("data")
 ).select("data.*")
+parsed_df = parsed_df.withColumn(
+    "timestamp",
+    to_timestamp(from_unixtime(col("timestamp")))
+)
 
 # Rule 1: High amount
-fraud_df = parsed_df.filter(col("amount") > 4000)
+high_amount_df = parsed_df.filter(col("amount") > 4000)
 
-query = fraud_df.writeStream \
-    .format("console") \
+# Rule 2: Multiple transactions in short time
+velocity_df = parsed_df \
+    .withWatermark("timestamp", "1 minute") \
+    .groupBy(
+        col("user_id"),
+        window(col("timestamp"), "30 seconds")
+    ) \
+    .agg(count("*").alias("txn_count")) \
+    .filter(col("txn_count") > 3)
+
+# Joining results
+
+high_amount_flag_df = high_amount_df.select(
+    col("user_id"),
+    col("amount"),
+    lit(None).cast("int").alias("txn_count"),
+    col("timestamp")
+).withColumn("fraud_type", lit("HIGH_AMOUNT"))
+
+velocity_flag_df = velocity_df.select(
+    col("user_id"),
+    lit(None).cast("double").alias("amount"),
+    col("txn_count"),
+    col("window").getField("start").alias("timestamp")
+).withColumn("fraud_type", lit("HIGH_VELOCITY"))
+
+final_fraud_df = high_amount_flag_df.unionByName(velocity_flag_df)
+
+query = final_fraud_df.writeStream \
+    .foreachBatch(write_to_postgres) \
     .outputMode("append") \
     .start()
 
